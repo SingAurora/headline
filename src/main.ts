9f7d544c3e9a119cff5c1fda0a8426b1915abc72
fancyday <2414786389@qq.com>
2022-07-28 22:10:32 +0800

    complete the project's initialization

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import 'amfe-flexible'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(Vant).use(store).use(router).mount('#app')
